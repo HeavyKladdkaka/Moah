ac2448ecebc807c55e84b84539ea194cf61a85d3
oscarthorwid <id15otd@cs.umu.se>
2019-05-09 14:56:45 +0200

    chatt view and no-games-available

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ import {
   ReactiveVar
 } from 'meteor/reactive-var';
 
-import './profile.html';
+import './no-games-available.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
