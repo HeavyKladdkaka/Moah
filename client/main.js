d281beb7820a1b5bb6aaf43a7fadf4a063bed91e
oscarthorwid <id15otd@cs.umu.se>
2019-05-14 09:33:20 +0200

    router main.js added and new main.html

--- client/main.js
+++ client/main.js
@@ -1,26 +1,109 @@
-import {
-  Template
-} from 'meteor/templating';
-import {
-  ReactiveVar
-} from 'meteor/reactive-var';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
-import './home.html';
+import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+FlowRouter.route('/', {
+  name: 'hej',
+  action(){
+    BlazeLayout.render('iphone');
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
+FlowRouter.route('/joinGame', {
+  name: 'joinGame',
+  action(){
+    BlazeLayout.render('iphone', {main: 'joinGame'});
+  }
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+FlowRouter.route('/profile', {
+  name: 'profile',
+  action(){
+    BlazeLayout.render('iphone', {main: 'profile'});
+  }
+});
+
+FlowRouter.route('/chatt', {
+  name: 'chatt',
+  action(){
+    BlazeLayout.render('iphone', {main: 'chatt'});
+  }
+});
+
+FlowRouter.route('/achievements', {
+  name: 'achievements',
+  action(){
+    BlazeLayout.render('iphone', {main: 'achievements'});
+  }
+});
+
+FlowRouter.route('/achievement', {
+  name: 'achievement',
+  action(){
+    BlazeLayout.render('iphone', {main: 'achievement'});
+  }
+});
+
+FlowRouter.route('/chooseAvatar', {
+  name: 'chooseAvatar',
+  action(){
+    BlazeLayout.render('iphone', {main: 'chooseAvatar'});
+  }
+});
+
+FlowRouter.route('/waitingForPlayer', {
+  name: 'waitingForPlayer',
+  action(){
+    BlazeLayout.render('iphone', {main: 'waitingForPlayer'});
+  }
+});
+
+FlowRouter.route('/im-ready', {
+  name: 'im-ready',
+  action(){
+    BlazeLayout.render('iphone', {main: 'im-ready'});
+  }
+});
+
+FlowRouter.route('/readyForGame', {
+  name: 'ready-for-game',
+  action(){
+    BlazeLayout.render('iphone', {main: 'readyForGame'});
+  }
+});
+
+FlowRouter.route('/inGame', {
+  name: 'inGame',
+  action(){
+    BlazeLayout.render('iphone', {main: 'inGame'});
+  }
+});
+
+FlowRouter.route('/inGameKeyboard', {
+  name: 'inGameKeyboard',
+  action(){
+    BlazeLayout.render('iphone', {main: 'inGameKeyboard'});
+  }
+});
+
+FlowRouter.route('/inGameChatt', {
+  name: 'inGameChatt',
+  action(){
+    BlazeLayout.render('iphone', {main: 'inGameChatt'});
+  }
+});
+
+FlowRouter.route('/inGameAnswers', {
+  name: 'inGameAnswers',
+  action(){
+    BlazeLayout.render('iphone', {main: 'inGameAnswers'});
+  }
+});
+
+FlowRouter.route('/inGameHighscore', {
+  name: 'inGameHighscore',
+  action(){
+    BlazeLayout.render('iphone', {main: 'inGameHighscore'});
+  }
 });
